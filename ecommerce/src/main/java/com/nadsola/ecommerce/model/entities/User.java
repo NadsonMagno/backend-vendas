c08373a7e05a48a5fb98209b6f30cd0ccc9de601
NadsonMagno <143035086+NadsonMagno@users.noreply.github.com>
2025-02-04 07:15:38 -0300

    criando a base de dados temporária

--- ecommerce/src/main/java/com/nadsola/ecommerce/model/entities/User.java
+++ ecommerce/src/main/java/com/nadsola/ecommerce/model/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +19,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+    
     private String password;
     private String phone;
     private LocalDate birthDate;
@@ -32,7 +36,10 @@ public class User {
     public User(Long id, String name, String email, String password, String phone, LocalDate birthDate) {
         this.id = id;
         this.name = name;
+
+        
         this.email = email;
+
         this.password = password;
         this.phone = phone;
         this.birthDate = birthDate;
