0400bc028cfff0964565fa84ea7f7aeb94f440f9
NadsonMagno <143035086+NadsonMagno@users.noreply.github.com>
2025-02-04 22:16:09 -0300

    Criando uma consulta ao banco de dados

--- ecommerce/src/main/java/com/nadsola/ecommerce/model/controllers/ProductController.java
+++ ecommerce/src/main/java/com/nadsola/ecommerce/model/controllers/ProductController.java
@@ -1,17 +1,28 @@
 package com.nadsola.ecommerce.model.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nadsola.ecommerce.model.entities.Product;
+import com.nadsola.ecommerce.repositories.ProductRepository;
 
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
     @GetMapping  
     public String teste(){
-        return "ola mundo!";
+
+      Optional<Product> result  = productRepository.findById(1L);
+      Product product = result.get();
+        return product.getName();
+
     }
 
 
